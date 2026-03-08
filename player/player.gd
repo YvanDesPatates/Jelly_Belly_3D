6357494bb3f3cb176b7495e2a7fe22678f395faa
yvan <yvis99@gmail.com>
2026-03-08 20:47:27 +0100

    player's movments

--- player/player.gd
+++ player/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 
+const SPEED = 5.5
 var x_sensitivity = 0.4#0.2 is very slow while 0.9 is fast
 var y_sensitivity = 0.2
 var max_y_rotation = 60
@@ -16,3 +17,12 @@ func _unhandled_input(event: InputEvent) -> void:
 		
 	if event.is_action_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _physics_process(delta: float) -> void:
+	var input_direction_2d = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var input_direction_3d = Vector3(input_direction_2d.x, 0.0 , input_direction_2d.y)
+	var direction = transform.basis * input_direction_3d
+	
+	velocity.x = direction.x * SPEED
+	velocity.z = direction.z * SPEED
+	move_and_slide()

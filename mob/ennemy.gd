5b178c1426a6290b8eeb279e33d815cd931336a0
ROUX YVAN <yvan.roux@inrae.fr>
2026-03-13 13:19:31 +0100

    enemmies dies

--- mob/ennemy.gd
+++ mob/ennemy.gd
@@ -2,8 +2,10 @@ extends RigidBody3D
 
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
 @onready var player: Node3D = get_node("/root/Game/Player")
+@onready var GAME: Node3D = get_node("/root/Game")
 
 const SPEED = 3
+var life = 4
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	look_at(player.global_position)
@@ -11,4 +13,12 @@ func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 
 
 func take_damage():
-	animation_player.play("hurt")
+	life -= 1
+	if life == 0:
+		queue_free()
+		const SMOKE_PUFF = preload("uid://cjk3frr43yesb")
+		var smoke: Node3D = SMOKE_PUFF.instantiate()
+		smoke.global_position = global_position
+		GAME.add_child(smoke)
+	else :
+		animation_player.play("hurt")
